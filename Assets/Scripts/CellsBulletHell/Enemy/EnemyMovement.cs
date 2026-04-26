c0d654e0e2b4b53b2b9d3e3f112da806814b9132
GlitchyMuon <glitchy.muon@proton.me>
2026-04-26 13:57:17 +0200

    Changed Camera and experimenting on it, added Titlescreen scene, bg, added lab bg

--- Assets/Scripts/CellsBulletHell/Enemy/EnemyMovement.cs
+++ Assets/Scripts/CellsBulletHell/Enemy/EnemyMovement.cs
@@ -2,15 +2,70 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _rotationSpeed;
+
+    private Rigidbody2D _rigidbody;
+    private PlayerAwarenessController _playerAwarenessController;
+    private Vector2 _targetDirection;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+    }
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+    private void UpdateTargetDirection()
+    {
+        if (_playerAwarenessController.AwareOfPlayer)
+        {
+            _targetDirection = _playerAwarenessController.DirectionToPlayer;
+        }
+        else
+        {
+            _targetDirection = Vector2.zero;
+            //maybe change later to a random Vector2 between the bounds of the Canvas
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+        _rigidbody.SetRotation(rotation);
+    }
+
+    private void SetVelocity()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            _rigidbody.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            _rigidbody.linearVelocity = transform.up * _speed;
+        }
     }
 }
+
