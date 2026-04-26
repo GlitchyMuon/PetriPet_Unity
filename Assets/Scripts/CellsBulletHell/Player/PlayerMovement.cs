11d797225a3eae591179eea78ae4d10531811b10
GlitchyMuon <glitchy.muon@proton.me>
2026-04-26 13:57:17 +0200

    Changed Camera and experimenting on it, added Titlescreen scene, bg, added lab bg

--- Assets/Scripts/CellsBulletHell/Player/PlayerMovement.cs
+++ Assets/Scripts/CellsBulletHell/Player/PlayerMovement.cs
@@ -9,11 +9,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
-    private Vector2 _smoothMovementInput;
+    private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
+    private Camera _camera;
 
     [SerializeField]
     private float _speed;
+
+    /*  [SerializeField]
+     private float _rotationSpeed; */
+    [SerializeField]
+    private float _screenBorder;
 
     private void Awake()
     {
@@ -23,13 +29,41 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _smoothMovementInput = Vector2.SmoothDamp(_smoothMovementInput, _movementInput, ref _movementInputSmoothVelocity, 0.1f);
+        _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _movementInputSmoothVelocity, 0.1f);
 
-        _rigidbody.linearVelocity = _smoothMovementInput * _speed;
+        _rigidbody.linearVelocity = _smoothedMovementInput * _speed;
 
 
     }
 
+    private void PreventPlayerGoingOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if ((screenPosition.x < _screenBorder && _rigidbody.linearVelocityX < 0) ||
+            (screenPosition.x > _camera.pixelWidth - _screenBorder && _rigidbody.linearVelocityX > 0))
+        {
+            _rigidbody.linearVelocity = new Vector2(0, _rigidbody.linearVelocity.y);
+        }
+
+        if ((screenPosition.y < _screenBorder && _rigidbody.linearVelocityY < 0) ||
+            (screenPosition.y > _camera.pixelHeight - _screenBorder && _rigidbody.linearVelocity.y > 0)) //_rigidbody.linearVelocity.y ?
+        {
+            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocityX, 0);
+        }
+    }
+
+    /* private void RotateInDirectionOfInput()
+    {
+        if (_movementInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+            _rigidbody.MoveRotation(rotation);
+        }
+    }
+ */
     private void OnMove(InputValue inputValue)
     {
         _movementInput = inputValue.Get<Vector2>();
